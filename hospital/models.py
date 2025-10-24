d5642860743fc4dc778b07eabfc8e7606ba2a265
Oleksandr Skoromnov <solalex005@gmail.com>
2025-10-24 17:17:29 +0300

    added basic models: worker, patients, specialty, task_type, task

--- hospital/models.py
+++ hospital/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Specialty(models.Model):
+    name = models.CharField(
+        max_length=24,
+        unique=True,
+    )
+
+
+class Worker(AbstractUser):
+    specialty = models.ForeignKey(
+        Specialty,
+        related_name="workers",
+        on_delete=models.DO_NOTHING
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+    )
+
+
+class Patient(models.Model):
+    first_name = models.CharField(
+        max_length=64,
+    )
+    last_name = models.CharField(
+        max_length=64,
+    )
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = 'LOW', 'Low'
+        MEDIUM = 'MEDIUM', 'Medium'
+        HIGH = 'HIGH', 'High'
+        CRITICAL = 'CRITICAL', 'Critical'
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    planned_on = models.DateTimeField()
+    is_canceled = models.BooleanField(default=False)
+    priority = models.CharField(
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        related_name="tasks",
+        on_delete=models.SET_NULL
+    )
+    assignees = models.ManyToManyField(
+        Worker,
+        related_name="tasks",
+    )
+    patients = models.ManyToManyField(
+        Patient,
+        related_name="tasks",
+    )
